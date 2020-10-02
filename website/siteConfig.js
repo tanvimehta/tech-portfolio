6db9bf4e0940f8f4ec8b117f8be44772705a595c
Tanvi Mehta <tanvimmehta@gmail.com>
2020-10-02 12:11:07 -0700

    Added personal info

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
+    title: 'Tanvi Mehta', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://tanvimehta.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'https://www.linkedin.com/in/tanvimmehta',
         twitter: '',
-        github: '',
+        github: 'https://github.com/tanvimehta',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
