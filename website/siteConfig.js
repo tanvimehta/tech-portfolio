7adc6c7e48e7271d7ea706441f63ac99ec4b23df
Tanvi Mehta <tanvimmehta@gmail.com>
2020-10-02 12:31:18 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Tanvi Mehta', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: 'Work in progress!',
     profilePhoto: 'img/profile.png',
     url: 'https://tanvimehta.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
