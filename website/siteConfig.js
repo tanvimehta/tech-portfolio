8fcedbdd5a8d8424fedf1ed62afc463c9a5f1c79
Tanvi Mehta <tanvimmehta@gmail.com>
2020-10-02 12:42:13 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Tanvi Mehta', // Title for your website.
     tagline: 'Work in progress!',
-    profilePhoto: 'img/Tanvi-Mehta.png',
+    profilePhoto: 'img/profile.png',
     url: 'https://tanvimehta.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
